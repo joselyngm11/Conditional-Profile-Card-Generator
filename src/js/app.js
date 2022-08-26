16bc1ee9396dbff0597d71193c504b95f360708e
joselyngm11 <joselyngm.47@gmail.com>
2022-08-26 22:55:08 +0000

    Primeras 5 casillas

--- src/js/app.js
+++ src/js/app.js
@@ -28,17 +28,23 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let name = variables.name ? variables.name : "Nombre";
+  let lastname = variables.lastname ? variables.lastname : "Apellido";
+  let socialMediaPosition = variables.socialMediaPosition
+    ? variables.socialMediaPosition
+    : "position-right";
+  let twitter = variables.twitter ? variables.twitter : "4geeksacademy";
+  let github = variables.github ? variables.github : "4geeksacademy";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${name} ${lastname}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+          <ul class=${socialMediaPosition}>
+            <li><a href= "https://twitter.com/${twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
           </ul>
@@ -61,7 +67,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
